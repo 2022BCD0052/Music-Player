02598bb01ce2c9ad1e08da94a6c0c9ff58e8239f
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2024-12-15 00:38:27 +0530

    add songs

---
+++ src/data/songWithCategory.js
@@ -0,0 +1,8 @@
+import { recommendedSongs } from "./songs";
+
+export const  songWithCategory = [
+    {
+        title : "Recommended For You",
+        songs : recommendedSongs,
+    }
+]
---
+++ src/data/songs.js
@@ -0,0 +1,53 @@
+export const recommendedSongs = [
+    {
+        url: 'https://lwvnwggpxcpdqhgsebpa.supabase.co/storage/v1/object/public/Musico-Player/Aayega%20Maza%20Ab%20Barsaat%20Ka(Remix)%20Dj%20Anil%20Thakur%20Akshay%20Kumar%20Priyanka%20Chopra%20Alka%20Yagnik%20Mix%202024(MP3_160K).mp3', // Load media from the network
+        title: 'Aayega Maza Ab Barsaat Ka(Remix)',
+        artist: 'Alka Yagnik',
+        album: 'Dj Anil Thakur',
+        genre: 'love',
+        date: '2014-05-20T07:00:00+00:00', // RFC 3339
+        artwork: 'https://pagalfree.com/images/128Aaye%20Ho%20Meri%20Zindagi%20Mein%20%20%20Male%20-%20Raja%20Hindustani%20128%20Kbps.jpg', // Load artwork from the network
+        duration: 402 // Duration in seconds
+    },
+    {
+        url: 'https://lwvnwggpxcpdqhgsebpa.supabase.co/storage/v1/object/public/Musico-Player/Aayega%20Maza%20Ab%20Barsaat%20Ka(Remix)%20Dj%20Anil%20Thakur%20Akshay%20Kumar%20Priyanka%20Chopra%20Alka%20Yagnik%20Mix%202024(MP3_160K).mp3', // Load media from the network
+        title: 'Aayega Maza Ab Barsaat Ka(Remix)',
+        artist: 'Alka Yagnik',
+        album: 'Dj Anil Thakur',
+        genre: 'love',
+        date: '2014-05-20T07:00:00+00:00', // RFC 3339
+        artwork: 'https://pagalfree.com/images/128Aayega%20Maza%20Ab%20Barsaat%20Ka%20-%20Andaaz.jpg', // Load artwork from the network
+        duration: 402 // Duration in seconds
+    },
+    {
+        url: 'https://lwvnwggpxcpdqhgsebpa.supabase.co/storage/v1/object/public/Musico-Player/Aayega%20Maza%20Ab%20Barsaat%20Ka(Remix)%20Dj%20Anil%20Thakur%20Akshay%20Kumar%20Priyanka%20Chopra%20Alka%20Yagnik%20Mix%202024(MP3_160K).mp3', // Load media from the network
+        title: 'Ghoonghat Ki Aadh Se',
+        artist: 'Alka Yagnik',
+        album: 'Dj Anil Thakur',
+        genre: 'sadabahar',
+        date: '2014-05-20T07:00:00+00:00', // RFC 3339
+        artwork: 'https://pagalfree.com/images/128Ghoonghat%20Ki%20Aadh%20Se%20-%20Hum%20Hain%20Rahi%20Pyar%20Ke%20128%20Kbps.jpg', // Load artwork from the network
+        duration: 402 // Duration in seconds
+    },
+    {
+        url: '            https://lwvnwggpxcpdqhgsebpa.supabase.co/storage/v1/object/public/Musico-Player/In%20Akhon%20Ki%20Masti%20Ke%20_%20Asha%20Bhosle%20_%20Lyrical%20Video%20_%20Rekha%20Songs%20_%20Umrao%20Jaan%20_%20Ghazal%20Romantic(MP3_160K).mp3?t=2024-12-14T18%3A50%3A36.034Z', // Load media from the network
+        title: 'In Akhon Ki Masti Ke _ Asha Bhosle',
+        artist: 'Umrao Jaan',
+        album: 'Dj Anil Thakur',
+        genre: 'Romantic',
+        date: '2014-05-20T07:00:00+00:00', // RFC 3339
+        artwork: 'https://pagalfree.com/images/128Jane%20Kyun%20Log%20Mujhe%20Baar%20Baar%20-%20Naya%20Safar%20128%20Kbps.jpg', // Load artwork from the network
+        duration: 330 // Duration in seconds
+    },
+    {
+        url: 'https://lwvnwggpxcpdqhgsebpa.supabase.co/storage/v1/object/public/Musico-Player/Jane%20Kyun%20Log%20Lyrical%20Video%20_%20Dil%20Chahta%20Hai%20_%20Udit%20Narayan_%20Alka%20Yagnik%20_%20Amir%20Khan_%20Preity%20Zinta(MP3_160K).mp3', // Load media from the network
+        title: 'Jane Kyun Log Lyrical Video _ Dil Chahta Hai',
+        artist: ' Udit Narayan',
+        album: 'Preity Zinta',
+        genre: 'sadabahar',
+        date: '2014-05-20T07:00:00+00:00', // RFC 3339
+        artwork: 'https://pagalfree.com/images/128Jane%20Kyun%20Log%20Mujhe%20Baar%20Baar%20-%20Naya%20Safar%20128%20Kbps.jpg', // Load artwork from the network
+        duration: 240 // Duration in seconds
+    },
+
+]
