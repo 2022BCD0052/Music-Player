076fe4e3dda81edb04c496576865c53f2b7d0352
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2024-12-14 23:04:25 +0530

    `Updated DrawerContent and DrawerNavigator to use DrawerItem and custom styles`

--- src/navigation/CustomeDrawerContent.jsx
+++ src/navigation/CustomeDrawerContent.jsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { DrawerContentScrollView } from '@react-navigation/drawer'
-import { iconSizes, spacing } from '../screen/constants/dimensions'
+import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer'
+import { FontSize, iconSizes, spacing } from '../screen/constants/dimensions'
 import AntDesign from "react-native-vector-icons/AntDesign";
+import FontAwersome from "react-native-vector-icons/FontAwesome";
 import Octions from "react-native-vector-icons/Octicons";
 import { colors } from '../screen/constants/colors';
+import { fontFamilies } from '../screen/constants/fonts';
 
 const CustomeDrawerContent = (props) => {
   const isDarkMode = true
@@ -23,8 +25,19 @@ const CustomeDrawerContent = (props) => {
 
         </View>
 
-        <View>
-          
+        <View style={styles.drawerIconContainer}>
+          <DrawerItem
+          label={"Profile"}
+          icon={()=>(<FontAwersome name="user" 
+            size={iconSizes.md}
+            color={colors.iconPrimary}
+          />)}
+          labelStyle={styles.labelStyle}
+          style={styles.Draweritem}
+
+
+          />
+
         </View>
 
       </DrawerContentScrollView>
@@ -43,6 +56,18 @@ const styles = StyleSheet.create({
     flexDirection :"row",
     justifyContent :"space-between",
     alignItems:"center"
+  },
+  drawerIconContainer:{
+    marginVertical:spacing.md
+  },
+  labelStyle:{
+    fontSize:FontSize.md,
+    color:colors.textPrimary,
+    fontFamily :fontFamilies.medium
+  },
+  Draweritem:{
+    marginVertical :spacing.sm
+
   }
 
 })
--- src/navigation/DrawerNavigator.jsx
+++ src/navigation/DrawerNavigator.jsx
@@ -2,18 +2,23 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import StackNavigation from './StackNavigation';
+import { colors } from '../screen/constants/colors';
 import CustomeDrawerContent from './CustomeDrawerContent';
 
 const Drawer = createDrawerNavigator();
 const DrawerNavigator = () => {
   return (
    <Drawer.Navigator 
-   defaultStatus='open'
    screenOptions={{
     headerShown: false,
-    drawerType :"slide",
+    // drawerType :"slide",
+    drawerStyle: {
+      backgroundColor: colors.background,
+      width: 200,
+      },
   }}
-  drawerContent={(props) => <CustomeDrawerContent {...props} />}
+  drawerContent={(props)=><CustomeDrawerContent{...props}/>}
+
    >
     <Drawer.Screen name="Home" component={StackNavigation} />
    </Drawer.Navigator>
@@ -22,6 +27,4 @@ const DrawerNavigator = () => {
 
 export default DrawerNavigator
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({})
