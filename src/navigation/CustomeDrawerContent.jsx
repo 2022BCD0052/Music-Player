af278f7cdce90d59bbc7f511b6c73737127f6b5e
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2024-12-14 22:45:41 +0530

    update

--- src/navigation/CustomeDrawerContent.jsx
+++ src/navigation/CustomeDrawerContent.jsx
@@ -1,34 +1,44 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { DrawerContentScrollView } from '@react-navigation/drawer'
-import { colors } from '../screen/constants/colors'
 import { iconSizes, spacing } from '../screen/constants/dimensions'
 import AntDesign from "react-native-vector-icons/AntDesign";
 import Octions from "react-native-vector-icons/Octicons";
+import { colors } from '../screen/constants/colors';
 
-const CustomeDrawerContent = () => {
+const CustomeDrawerContent = (props) => {
+  const isDarkMode = true
+
   return (
-    <View>
-      <DrawerContentScrollView style={styles.HeaderIconcontainer}>
-        <View>
+     <DrawerContentScrollView style={styles.container}>
+        <View style={styles.HeaderIconcontainer}>
         <TouchableOpacity>
-            <AntDesign name="close" size={iconSizes.lg} color={colors.textSecondary} />
+            <AntDesign name="close" size={iconSizes.lg} color={colors.iconPrimary} />
 
           </TouchableOpacity>
           <TouchableOpacity>
-            <Octions name="moon" size={iconSizes.lg} color={colors.textSecondary} />
+            <Octions name={isDarkMode ? "sun" :"moon"} size={iconSizes.lg} color={colors.iconPrimary} />
 
           </TouchableOpacity>
 
         </View>
 
       </DrawerContentScrollView>
-    </View>
   )
 }
 
 export default CustomeDrawerContent
 
 const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    backgroundColor: colors.background,
+    padding: spacing.md,
+  },
+  HeaderIconcontainer:{
+    flexDirection :"row",
+    justifyContent :"space-between",
+    alignItems:"center"
+  }
 
 })
--- src/navigation/DrawerNavigator.jsx
+++ src/navigation/DrawerNavigator.jsx
@@ -1,29 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import StackNavigation from "./StackNavigation";
-import { colors } from "../screen/constants/colors";
-import CustomeDrawerContent from "./CustomeDrawerContent";
-import { spacing } from "../screen/constants/dimensions";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import StackNavigation from './StackNavigation';
+import CustomeDrawerContent from './CustomeDrawerContent';
 
 const Drawer = createDrawerNavigator();
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerShown: false,
-        drawerType: "slide",
-        drawerStyle: {
-          backgroundColor: colors.background,
-        },
-      }}
-      drawerContent={(props) => <CustomeDrawerContent {...props} />}
-    >
-      <Drawer.Screen name="Home" component={StackNavigation} />
-    </Drawer.Navigator>
-  );
-};
+   <Drawer.Navigator 
+   defaultStatus='open'
+   screenOptions={{
+    headerShown: false,
+    drawerType :"slide",
+  }}
+  drawerContent={(props) => <CustomeDrawerContent {...props} />}
+   >
+    <Drawer.Screen name="Home" component={StackNavigation} />
+   </Drawer.Navigator>
+  )
+}
 
-export default DrawerNavigator;
+export default DrawerNavigator
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+})
